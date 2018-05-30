a460ef566b73c6772d806b40b4ba25458b60e0c2
Patrick Klostermann <dev@netflair.at>
2018-05-30 22:47:37 +0200

    imports of Angular Modules

--- dating-app/src/app/app.module.ts
+++ dating-app/src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDitKVtJvhK79WG7_h2H7QfVwqUn_aNTOs',
+  authDomain: 'cr-dating-app.firebaseapp.com',
+  databaseURL: 'https://cr-dating-app.firebaseio.com',
+  projectId: 'cr-dating-app',
+  storageBucket: '',
+  messagingSenderId: '342295876395'
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
